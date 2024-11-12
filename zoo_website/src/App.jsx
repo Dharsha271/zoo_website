e7da3aaa4ba0851a1fe5dd476f72e4556bec2e3f
Dharsha271 <146057550+Dharsha271@users.noreply.github.com>
2024-11-12 18:01:43 +0530

    Add files via upload

---
+++ zoo_website/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+let login = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () =>{
+    login.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-btn").onclick = () =>{
+    login.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    login.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+export default App
